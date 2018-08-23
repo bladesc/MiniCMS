4d95f81202ee6ec5648f9b4a5dca4695728deba8
bladesc <bladesc@interia.pl>
2018-08-23 19:21:37 +0200

    administrtor menu content

--- resources/views/administrator/index.blade.php
+++ resources/views/administrator/index.blade.php
@@ -1,1 +1,15 @@
 @extends('administrator.layout.layout')
+<div id="left-bar">
+    <h2>Navigation</h2>
+    <section>
+        <ul>
+            <li><a href="">Menu</a></li>
+            <li><a href="">Cms</a></li>
+            <li><a href="">Galeria</a></li>
+            <li><a href="">Seo</a></li>
+        </ul>
+    </section>
+</div>
+<div id="right-bar">
+
+</div>
--- resources/views/administrator/layout/layout.blade.php
+++ resources/views/administrator/layout/layout.blade.php
@@ -16,9 +16,8 @@
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
     <link href="https://fonts.googleapis.com/css?family=Ubuntu:400,500,700" rel="stylesheet">
     <!-- Styles -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    <link href="{{ asset('css/style.css') }}" rel="stylesheet">
+
+    <link href="{{ asset('css/admin.css') }}" rel="stylesheet">
 
 </head>
 <body>
