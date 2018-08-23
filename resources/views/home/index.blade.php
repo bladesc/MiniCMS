8ee5f8e29e73de2ae50113a06c82776cbfbf6b68
bladesc <bladesc@interia.pl>
2018-08-23 15:24:46 +0200

    created skeleton for page index

---
+++ resources/views/home/index.blade.php
@@ -0,0 +1,10 @@
+@extends('home.layout.layout')
+
+@include('home.main.header')
+@include('home.main.slider')
+@include('home.main.about')
+@include('home.main.services')
+@include('home.main.gallery')
+@include('home.main.contact')
+@include('home.main.map')
+@include('home.main.footer')
