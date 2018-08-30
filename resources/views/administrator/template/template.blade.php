9fdaa7185fa09e60b1cefb5c2eb3ca0ab0909de3
bladesc <bladesc@interia.pl>
2018-08-30 18:37:30 +0200

    adde views or template

--- resources/views/administrator/template/template.blade.php
+++ resources/views/administrator/template/template.blade.php
@@ -2,33 +2,41 @@
 
 @section('content')
     <a class="new right" href="{{route('admin.template.add.item', 1)}}">Add logo <i class="far fa-plus-square"></i></a>
-    <a class="new right" href="{{route('admin.template.add.item', 2)}}">Add header <i class="far fa-plus-square"></i></a>
+    <a class="new right" href="{{route('admin.template.add.item', 2)}}">Add header <i
+            class="far fa-plus-square"></i></a>
 
-    <section>
+
+    <div id="gallery">
         @foreach($templateImages as $templateImage)
-            <div>
-                <div class="img-container">
+            <div class="images">
+               {{-- <p>{{$templateImage->item_type}}</p>--}}
+                <img class="col-1 d-inline p-1 m-2 border" src="{{asset($templateImage->item_url)}}"
+                     alt="{{$templateImage->item_name}}"/>
+                <span>
                     {{$templateImage->item_name}}
-                    {{$templateImage->item_type}}
-                    <img src="{{asset($templateImage->item_url)}}">
-                </div>
-                <div class="form-container">
-                    <form action="{{route('admin.template.update.item', ['itemType' => $templateImage->item_type, 'itemId' => $templateImage->id])}}" method="post">
+                    <div class="form-container">
+                    <form
+                        action="{{route('admin.template.update.item', ['itemType' => $templateImage->item_type, 'itemId' => $templateImage->id])}}"
+                        method="post">
                         <input type="hidden" required name="id" value="{{$templateImage->id}}">
                         <input type="hidden" name="_token" value="<?php echo csrf_token(); ?>">
                         <button class="other">Change</button>
                     </form>
 
-                    <form action="{{route('admin.template.delete.item',['itemType' => $templateImage->item_type, 'itemId' => $templateImage->id])}}" method="post">
+                    <form
+                        action="{{route('admin.template.delete.item',['itemType' => $templateImage->item_type, 'itemId' => $templateImage->id])}}"
+                        method="post">
                         <input type="hidden" required name="id" value="{{$templateImage->id}}">
                         <input type="hidden" name="_token" value="<?php echo csrf_token(); ?>">
                         <button class="other">Delete</button>
                     </form>
                 </div>
+                </span>
             </div>
-
         @endforeach
-    </section>
+        <div class="foot-box"></div>
+    </div>
+
     @if(session()->has('message'))
         <div class="alert alert-success">
             {{ session()->get('message') }}
